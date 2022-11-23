b9f2a20aafb99fcf6e16de96a567b81cd395e04a
JC Grennan <jgrennan94@gmail.com>
2022-11-23 12:43:31 +0000

    screenshot test

--- shopping.js
+++ shopping.js
@@ -1,10 +1,15 @@
 const puppeteer = require("puppeteer")
 
 async function shop(searchTerm) {
-    
+
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto("https://www.sainsburys.co.uk/")
+
+    await page.click("#onetrust-accept-btn-handler")
+    await page.screenshot({path: "test.png"})
+
+    await browser.close()
 
 }
 
