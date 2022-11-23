ae24ed4f40cb126b735842f3e9b6a8fc9808bab2
JC Grennan <jgrennan94@gmail.com>
2022-11-23 12:40:25 +0000

    browser and page setup

--- shopping.js
+++ shopping.js
@@ -2,6 +2,10 @@ const puppeteer = require("puppeteer")
 
 async function shop(searchTerm) {
     
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto("https://www.sainsburys.co.uk/")
+
 }
 
 shop()
