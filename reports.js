063bcea226c39f58f7373d71cd77c1bb94a5590d
JC Grennan <jgrennan94@gmail.com>
2022-11-23 12:36:54 +0000

    browser and page setup

--- reports.js
+++ reports.js
@@ -2,6 +2,10 @@ const puppeteer = require("puppeteer")
 
 async function downloadReports() {
 
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto("https://www.sainsburys.co.uk/")
+
 }
 
 downloadReports()
