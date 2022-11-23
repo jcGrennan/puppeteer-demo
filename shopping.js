3b3165f372e14795f24cc427bfdf83f6cedc0f82
JC Grennan <jgrennan94@gmail.com>
2022-11-23 15:01:28 +0000

    navigated through search bar

--- shopping.js
+++ shopping.js
@@ -4,13 +4,20 @@ async function shop(searchTerm) {
 
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
+    page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4182.0 Safari/537.36")
     await page.goto("https://www.sainsburys.co.uk/")
-
+      
+    await page.waitForNetworkIdle()
     await page.click("#onetrust-accept-btn-handler")
-    await page.screenshot({path: "test.png"})
+
+    await page.waitForNetworkIdle()
+    await page.type("#term", searchTerm)
+    await Promise.all([page.click("[data-testid~=search-btn]"), page.waitForNavigation({waitUntil: "networkidle0"})])
+    await page.screenshot({path:"test.png", fullPage: true})
+  
 
     await browser.close()
 
 }
 
-shop()
+shop("chicken")
